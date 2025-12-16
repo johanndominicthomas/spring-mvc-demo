ef8f820bee81278ba944afca4207744079a91f6d
johanndominicthomas <johannthomaspala@gmail.com>
2025-12-16 17:43:13 +0530

    Added implementation class for TeacherService interface

---
+++ src/main/java/com/luminar/service/TeacherServiceImpl.java
@@ -0,0 +1,42 @@
+package com.luminar.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luminar.beans.TeacherBean;
+import com.luminar.dao.TeacherDao;
+
+@Service
+public class TeacherServiceImpl implements TeacherService {
+	
+	@Autowired
+	private TeacherDao teacherDao;
+
+	@Override
+	public void saveTeacher(TeacherBean teacher) {
+		teacherDao.insertTeacher(teacher);
+		
+	}
+
+	@Override
+	public int updateTeacher(TeacherBean teacher) {
+		return teacherDao.updateTeacherRecord(teacher);
+	}
+
+	@Override
+	public int deleteTeacher(int id) {
+		return teacherDao.removeTeacherById(id);
+	}
+
+	@Override
+	public List<TeacherBean> getTeacherDetails() {
+		return teacherDao.fetchAllTeachers();
+	}
+
+}
+
+/*
+ * Service is just a layer between Controller and Dao to hide the Dao methods.
+ */
