ef5d625c30a44d50bec49bcd1d15aed13c35d706
johanndominicthomas <johannthomaspala@gmail.com>
2025-12-16 18:33:56 +0530

    Added save() which adds teacher

--- src/main/java/com/luminar/controller/TeacherController.java
+++ src/main/java/com/luminar/controller/TeacherController.java
@@ -3,6 +3,8 @@ package com.luminar.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,4 +26,13 @@ public class TeacherController {
 	}
 	
 	//Model object is injected by spring when call comes to /teacher
+	
+	@PostMapping("/save")
+	public String save(@ModelAttribute("teacher") TeacherBean teacher) {
+		teacherService.saveTeacher(teacher);
+		return "redirect:/viewstudent";
+	}
+	
+	//redirect causes redirect to method mapped with viewstudent below
+	
 }
